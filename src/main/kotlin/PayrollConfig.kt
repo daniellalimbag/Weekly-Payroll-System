5fa4f73a577fc6ca821e1daa4f4b1330d971d5fd
Daniella Limbag <92845499+daniellalimbag@users.noreply.github.com>
2023-11-06 23:34:23 +0800

    Update PayrollConfig.kt

--- src/main/kotlin/PayrollConfig.kt
+++ src/main/kotlin/PayrollConfig.kt
@@ -1,2 +1,13 @@
-class PayrollConfig {
+data class PayrollConfig(
+    var dailyRate: Float = 500.0F,
+    var maxHours: Int = 0,
+    var workdays: Int = 2,
+    var defDayType: String = "Normal",
+    var defInTime: String = "0900",
+    var defOutTime: String = "0900"
+) {
+    // Initializer block
+    init {
+
+    }
 }
