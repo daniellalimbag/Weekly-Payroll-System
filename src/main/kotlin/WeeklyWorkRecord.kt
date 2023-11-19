0e80ed594d0562b140a1812507daab301e7da99a
Daniella Limbag <92845499+daniellalimbag@users.noreply.github.com>
2023-11-20 01:15:01 +0800

    Update WeeklyWorkRecord.kt

--- src/main/kotlin/WeeklyWorkRecord.kt
+++ src/main/kotlin/WeeklyWorkRecord.kt
@@ -1,6 +1,4 @@
-data class WeeklyWorkRecord (
-    var records: ArrayList<DailyWorkRecord> = ArrayList(7),
+data class WeeklyWorkRecord(
+    var records: MutableList<DailyWorkRecord> = ArrayList(7),
     var totalSalary: Float = 0.0f
-){
-
-}
+)
