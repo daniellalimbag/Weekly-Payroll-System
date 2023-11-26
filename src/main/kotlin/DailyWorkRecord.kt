ff59c3f7f8eb5c399886932b4f393f10314ec9d8
Daniella Limbag <92845499+daniellalimbag@users.noreply.github.com>
2023-11-27 02:07:15 +0800

    Update DailyWorkRecord.kt

--- src/main/kotlin/DailyWorkRecord.kt
+++ src/main/kotlin/DailyWorkRecord.kt
@@ -1,19 +1,20 @@
 data class DailyWorkRecord(
-    val payrollConfig: PayrollConfig,
-    var inTime: String = payrollConfig.defInTime,
-    var outTime: String = payrollConfig.defOutTime,
-    var dayType: String = payrollConfig.defDayType,
-    var regOvertimeHrs: Int = 0,
-    var nsHrs: Int = 0,
-    var nsOvertimeHrs: Int = 0,
-    var salary: Float = 0.0F,
-    var isNightShift: Boolean = false,
-    var isAbsent: Boolean,
-    var isRestDay: Boolean
-) {
-    init{
+        val payrollConfig: PayrollConfig,
+        var inTime: String = payrollConfig.defInTime,
+        var outTime: String = payrollConfig.defOutTime,
+        var dayType: String = payrollConfig.defDayType,
+        var regOvertimeHrs: Float = 0.0F,
+        var nsHrs: Float = 0.0F,
+        var nsOvertimeHrs: Float = 0.0F,
+        var salary: Float = 0.0F,
+        var isNightShift: Boolean = false,
+        var isAbsent: Boolean,
+        var isRestDay: Boolean
+)  {
+    init {
         val nInTime = inTime.toInt()
         var nOutTime = outTime.toInt()
+
         isAbsent = nInTime == nOutTime
         val workHours = payrollConfig.maxHours * 100 + 100
 
@@ -24,33 +25,52 @@ data class DailyWorkRecord(
                 "Special Non-Working Day" -> dayType = "Special Non-Working Day and Rest Day"
             }
         }
-        //Check if "OUT" time is after midnight (e.g., "0000")
+
         if (nOutTime < nInTime) {
             nOutTime += 2400
         }
-        //Check if the day is night shift
+
         isNightShift = nOutTime >= 2200
         var isOvertime: Boolean = nOutTime > (nInTime + workHours)
 
         if (!isNightShift && !isOvertime) {
-            nsHrs = 0
-            nsOvertimeHrs = 0
-            regOvertimeHrs = 0
+            nsHrs = 0.0F
+            nsOvertimeHrs = 0.0F
+            regOvertimeHrs = 0.0F
         }
         if (!isNightShift && isOvertime) {
-            nsHrs = 0
-            nsOvertimeHrs = 0
-            regOvertimeHrs = (nOutTime - (nInTime + workHours)) / 100
+            nsHrs = 0.0F
+            nsOvertimeHrs = 0.0F
+            val minutes = subtractMilitaryTimes(nOutTime, (nInTime + workHours))
+            regOvertimeHrs = minutes / 60.0F
         }
         if (isNightShift && !isOvertime) {
-            regOvertimeHrs = 0
-            nsOvertimeHrs = 0
-            nsHrs = (nOutTime - 2200) / 100
+            regOvertimeHrs = 0.0F
+            nsOvertimeHrs = 0.0F
+            nsHrs = subtractMilitaryTimes(nOutTime, 2200) / 60.0F
         }
         if (isNightShift && isOvertime) {
-            regOvertimeHrs = (2200 - (nInTime + workHours)) / 100
-            nsHrs = 0 //fix thissssssssssssssssssssssssssssss
-            nsOvertimeHrs = (nOutTime - 2200) / 100
+            val rMinutes = subtractMilitaryTimes(2200, (nInTime + workHours))
+            regOvertimeHrs = rMinutes / 60.0F
+            nsHrs = 0.0F // fix thissssssssssssssssssssssssssssss
+            nsOvertimeHrs = subtractMilitaryTimes(nOutTime, 2200) / 60.0F
         }
+
+        println("NS hours: $nsHrs")
+        println("OT hours: $regOvertimeHrs")
+        println("NSOT hours: $nsOvertimeHrs")
+    }
+
+    fun subtractMilitaryTimes(time1: Int, time2: Int): Int {
+        val hours1 = time1 / 100
+        val minutes1 = time1 % 100
+
+        val hours2 = time2 / 100
+        val minutes2 = time2 % 100
+
+        val totalMinutes1 = hours1 * 60 + minutes1
+        val totalMinutes2 = hours2 * 60 + minutes2
+
+        return totalMinutes1 - totalMinutes2
     }
 }
